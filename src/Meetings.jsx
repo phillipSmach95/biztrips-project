44014443e7f4b62cb2a509b5075f4f255b24e944
phillipSmach95 <smach.phillip@gmail.com>
2024-06-25 14:46:55 +0200

    changed db and fixed form

--- src/Meetings.jsx
+++ src/Meetings.jsx
@@ -2,63 +2,46 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getProducts } from "./services/productService";
 
-export default function Meetings(){
-    const [trips, setTrips] = useState([]);
-    useEffect(() => {
-      getProducts("trips").then((res) => {
-        setTrips(res);
-      });
-    }, []);
+export default function Meetings() {
+  const [meetings, setMeetings] = useState([]);
+  const [trips, setTrips] = useState([]);
+  useEffect(() => {
+    getProducts("meetings").then((res) => {
+      setMeetings(res);
+      getProducts("trips").then((res) => {setTrips(res)})
+    });
+  }, []);
 
-    function renderMeetings(m) {
-        
-        return(<>
-        
-        <h4>Meeting {m.title}</h4>
-        <p>{m.description}</p>
-        <div>
-                <Link to="/meetingform">
-                  edit meeting
-                </Link>
-              </div>
-        </>)
-}
-    function renderTrip(t) {
-      return (
-        <div className="product" key={t.id}>
-          <figure>
+  function renderMeetings(m) {
+    const trip = trips[m.tripId]?.title
+    return (
+      <>
+        <div className="product" key={m.id}>
+          <div>
+            <h2>{trip}</h2>
+            <h4>Meeting {m.title}</h4>
+            <p>{m.description}</p>
             <div>
-              <img src={"images/items/" + t.id + ".jpg"} alt="name " />
+              <Link to="/meetingform">edit meeting</Link>
             </div>
-            <figcaption>
-              <a href="#. . . ">{t.title}</a>
-              <div>
-                <span>
-                  {t.startTrip[2] + "-" + t.startTrip[1] + "-" + t.startTrip[0]}
-                </span>
-              </div>
-              <p>{t.description}</p>
-              <div>
-                {t.meetings.map(renderMeetings)}
-              </div>
-              
-            </figcaption>
-          </figure>
+          </div>
         </div>
-      );
-    }
-    return(
-        <>
-        <section id="filters">
-            <label htmlFor="month">Filter by Month:</label>{" "}
-            <select id="size">
-              <option value="">All months</option>
-              <option value="1">January</option>
-              <option value="2">February</option>
-              <option value="3">March</option>
-            </select>
-          </section>
-          {trips.map((t)=>renderTrip(t))} 
-        </>
-    )
+      </>
+    );
+  }
+
+  return (
+    <>
+      <section id="filters">
+        <label htmlFor="month">Filter by Month:</label>{" "}
+        <select id="size">
+          <option value="">All months</option>
+          <option value="1">January</option>
+          <option value="2">February</option>
+          <option value="3">March</option>
+        </select>
+      </section>
+      {meetings.map((m) => renderMeetings(m))}
+    </>
+  );
 }
--- src/services/productService.js
+++ src/services/productService.js
@@ -7,7 +7,22 @@ export async function getProducts(category) {
 }
 
 export async function getProduct(id) {
-  const response = await fetch(baseUrl + "products/" + id);
+  const response = await fetch(baseUrl + `products/${id}` );
+  if (response.ok) return response.json();
+  throw response;
+}
+export async function postProduct(product) {
+  const response = await fetch(baseUrl + "products/",{method:"post",body:product});
+  if (response.ok) return response.json();
+  throw response;
+}
+export async function putProduct(id,product) {
+  const response = await fetch(baseUrl + "products/" + id,{method:"put",body:product});
+  if (response.ok) return response.json();
+  throw response;
+}
+export async function deleteProduct(id) {
+  const response = await fetch(baseUrl + "products/" + id,{method:"delete"});
   if (response.ok) return response.json();
   throw response;
 }
