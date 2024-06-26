587e5ef25f08b51090af45d5de10a91ebf1796bf
phillipSmach95 <smach.phillip@gmail.com>
2024-06-26 13:08:20 +0200

    hashrouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter} from "react-router-dom"
+import {HashRouter as BrowserRouter} from "react-router-dom"
 import {createRoot} from "react-dom/client";
 import App from "./App";
 
