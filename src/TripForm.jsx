0664435513eb7352efbba3fb60b3c355dc860772
phillipSmach95 <smach.phillip@gmail.com>
2024-06-27 08:42:10 +0200

    form changes

--- src/TripForm.jsx
+++ src/TripForm.jsx
@@ -8,17 +8,20 @@ export default function TripForm() {
   const [trips, setTrips] = useState([])
   const [trip, setTrip] = useState()
   const {tripId}= useParams()
+  useEffect(()=>{
+    getProduct("trips",tripId).then((res)=>{setTrip(res)})
+  },[tripId])
 console.log(tripId);
   return (
     <>
     <div className="content">
-        <h1>Trip Modus</h1>
+        <h1>Trip Modus </h1>
 
       <form action="" method="post">
          <div>
 
           <label htmlFor="title">Title </label>
-          <input type="text" name="title" id="title"  />
+          <input type="text" name="title" id="title" value={trip.title}  />
           <label htmlFor="description">Description</label>
           <textarea rows={3} name="description" id="description" />
           <label htmlFor="startTrip">Start of Trip</label>
