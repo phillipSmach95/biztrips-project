57f716be447f9dfa792541db9ba9f3017ea438b6
phillipSmach95 <smach.phillip@gmail.com>
2024-06-30 18:32:37 +0200

    employees to form added fixed date issue loging result on submit

--- src/TripForm.jsx
+++ src/TripForm.jsx
@@ -2,52 +2,112 @@ import "./TripForm.css";
 import "./App.css";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { getProduct } from "./services/productService";
+import { getProduct, getProducts } from "./services/productService";
 
 export default function TripForm() {
-  const [trips, setTrips] = useState([])
-  const [trip, setTrip] = useState({})
-  const {tripId}= useParams()
-  console.log(Date(trip.startTrip));
-  useEffect(()=>{
-    getProduct("trips",1).then((res)=>{
-      console.log(res);
-      setTrip(res);
+
+  const [employees, setEmployees] = useState([])
+  const [title, setTitle] = useState("")
+  const [paticipants, setPaticipants] = useState([])
+  const [meetings, setMeetings] = useState([])
+  const [description, setDescription] = useState("")
+  const [startTripDate, setStartTripDate] = useState("")
+  const [startTripTime, setStartTripTime] = useState("")
+  const [endTripDate, setEndTripDate] = useState("")
+  const [endTripTime, setEndTripTime] = useState("")
+  const toStringDate = (date) => `${date[0]}-${String(date[1]).padStart(2, '0')}-${String(date[2]).padStart(2, '0')}`;
+  const toStringTime = (date) => `${String(date[3]).padStart(2, '0')}:${String(date[4]).padStart(2, '0')}`;
+  const toIntArray = (stringDate, stringTime) => {
+    const [year, month, day] = stringDate.split('-').map(Number);
+    const [hour, minute] = stringTime.split(':').map(Number);
+    const updatedValues = [year, month, day, hour, minute];
+    return updatedValues
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const body = {
+      title: title,
+      description: description,
+      startTrip: toIntArray(startTripDate, startTripTime),
+      endTrip: toIntArray(endTripDate, endTripTime),
+      paticipants: paticipants,
+      meetings: meetings,
+    }
+    console.log(body);
+  }
+
+  function showEmployees(e) {
+    let value = false;
+    if (paticipants.includes(e.id))
+      value = true;
+     const handleChange =(isParticipant)=>{
+      if (isParticipant){
+        setPaticipants([...paticipants, e.id])
+        console.log(paticipants);
+      }else{
+        setPaticipants(paticipants.filter((v)=>v !== e.id))
+        console.log(paticipants);
+      }
+     }
+    return (
+      <>
+        <label key={e.id} htmlFor={`employee${e.id}`}>
+          <input type="checkbox" name={`employee${e.id}`} id={`employee${e.id}`} onChange={(e)=>{handleChange(e.target.checked)}} checked={value}/>
+          {e.name}
+        </label>
+      </>
+    )
+  }
+
+  const { tripId } = useParams()
+  useEffect(() => {
+    getProducts("employees").then((res) => {
+      setEmployees(res)
+    })
+
+    getProduct("trips", tripId).then((res) => {
+      setDescription(res.description)
+      setTitle(res.title)
+      setStartTripDate(toStringDate(res.startTrip))
+      setStartTripTime(toStringTime(res.startTrip))
+      setEndTripDate(toStringDate(res.endTrip))
+      setEndTripTime(toStringTime(res.endTrip))
+      setPaticipants(res.paticipants)
+      setMeetings(res.meetings)
     });
-    
-  },[tripId])
-  console.log(trip.title);
-console.log(tripId);
+  }, [tripId])
+
   return (
     <>
-    <div className="content">
+      <div className="content">
         <h1>Trip Modus </h1>
 
-      <form action="" method="post">
-         <div> 
+        <form onSubmit={handleSubmit} >
+          <div>
 
-          <label htmlFor="title">Title </label>
-          <input type="text" name="title" id="title" value={trip.title}  />
-          <label htmlFor="description">Description</label>
-          <textarea rows={3} name="description" id="description" value={trip.description} />
-          <label htmlFor="startTrip" >Start of Trip</label>
-          <input type="date" name="startTrip" id="startTrip" value={Date(trip.startTrip)} />
-          <label htmlFor="endTrip">End of trip</label>
-          <input type="date" name="endTrip" id="endTrip" />
-         </div>
-        
-        <div className="checkbox-div">
-          <label htmlFor="employee1">
-            <input type="checkbox" name="employee1" id="employee1" />
-            Mitarbeiter 1
-          </label>
-          <label htmlFor="employee2">
-            <input type="checkbox" name="employee2" id="employee2" />
-            Mitarbeiter 2
-          </label>
-        </div>
-      </form>
-    </div>
+            <label htmlFor="title">Title </label>
+            <input type="text" name="title" id="title" onChange={(e) => { setTitle(e.target.value) }} value={title} />
+            <label htmlFor="description">Description</label>
+            <textarea rows={3} name="description" id="description" onChange={(e) => { setDescription(e.target.value) }} value={description} />
+            <label htmlFor="startTrip" >Start of Trip</label>
+            <div className="date-and-time">
+              <input type="date" name="startTrip" id="startTrip" onChange={(e) => { setStartTripDate(e.target.value); console.log(startTripDate); }} value={startTripDate} />
+              <input type="time" name="startTrip" id="startTrip" onChange={(e) => { setStartTripTime(e.target.value); console.log(startTripTime); }} value={startTripTime} />
+            </div>
+
+            <label htmlFor="endTrip">End of trip</label>
+            <div className="date-and-time">
+              <input type="date" name="endTrip" id="endTrip" onChange={(e) => { setEndTripDate(e.target.value) }} value={endTripDate} />
+              <input type="time" name="endTrip" id="endTrip" onChange={(e) => { setEndTripTime(e.target.value) }} value={endTripTime} />
+            </div>
+          </div>
+
+          <div className="checkbox-div">
+            {employees.map((e)=> showEmployees(e))}
+          </div>
+          <button type="submit">Save</button>
+        </form>
+      </div>
     </>
   );
 }
