a0faa234ab2a67748a2ff748756d421298a3ff95
phillipSmach95 <smach.phillip@gmail.com>
2024-07-02 15:59:17 +0200

    deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-// import { HashRouter as BrowserRouter} from "react-router-dom"
-import {BrowserRouter } from "react-router-dom"
+ import { HashRouter as BrowserRouter} from "react-router-dom"
+//import {BrowserRouter } from "react-router-dom"
 import {createRoot} from "react-dom/client";
 import App from "./App";
 
