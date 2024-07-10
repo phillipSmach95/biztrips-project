fee1ebf48afb0d2a5a13370e91c3158bae5ea647
phillipSmach95 <smach.phillip@gmail.com>
2024-07-10 19:49:21 +0200

    server.ts change

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
+server.use("/api/server");
 server.use(router);
 server.listen(3001, () => {
   console.log('JSON Server is running');
--- src/services/productService.js
+++ src/services/productService.js
@@ -1,5 +1,5 @@
 //const baseUrl = process.env.REACT_APP_API_BASE_URL
-const baseUrl = "https://biztrips-project.vercel.app/api/";
+const baseUrl = "https://biztrips-project.vercel.app/";
 export async function getProducts(category) {
   const response = await fetch(baseUrl + category, {
     headers: {
